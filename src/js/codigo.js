f6c5252fb4c19cdd1df378f153665ba94b84760c
Iara <iarajfares@gmail.com>
2023-12-06 01:08:06 -0300

    delete codigo.js

--- src/js/codigo.js
+++
@@ -1,4 +0,0 @@
-// const nombre = document.getElementById('editarNombre');
-// const descripcion = document.getElementById('editarDescripcion');
-// const precio = document.getElementById('editarPrecio');
-// const btn = 
